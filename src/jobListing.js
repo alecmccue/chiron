8541a0fae0f632f06a12bb6bafa4d7e5465b0914
Tommy Qu <tommyqu03@gmail.com>
2024-03-23 15:16:16 -0400

    fixed syntax

--- src/jobListing.js
+++ src/jobListing.js
@@ -1,22 +1,4 @@
-// require('dotenv').config();
-// const axios = require('axios');
 const PORT = 3001;
-// const apiKey = process.env.ADZUNA_API_KEY;
-// const apiID = process.env.ADZUNA_API_ID;
-
-// function fetchJobs(country = "us", resultsPerPage = 5, page = 1) {
-//     const apiUrl = `https://api.adzuna.com/v1/api/jobs/${country}/search/${page}?app_id=${apiID}&app_key=${apiKey}&results_per_page=${resultsPerPage}`;
-
-//     return axios.get(apiUrl)
-//         .then(response => {
-//         console.log(response.data);
-//         return response.data;
-//         })
-//         .catch(error => {
-//         console.error(error);
-//         throw error;
-//         });
-// }
 
 export function getJob(country = "us", resultsPerPage = 5, page = 1) {
     const queryParams = new URLSearchParams({
@@ -42,5 +24,4 @@ export function getJob(country = "us", resultsPerPage = 5, page = 1) {
         });
 }
 
-getJob();
 
