a00abe5677a74da0740e81cf6b4e0b2e908a3b52
Tommy Qu <tommyqu03@gmail.com>
2024-03-23 14:39:56 -0400

    created express server

--- jobListing.jsx
+++ jobListing.jsx
@@ -1,18 +1,8 @@
 require('dotenv').config();
 const axios = require('axios');
-
+const PORT = 3001;
 const apiKey = process.env.ADZUNA_API_KEY;
 const apiID = process.env.ADZUNA_API_ID;
-
-const apiUrl = `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${apiID}&app_key=${apiKey}&results_per_page=5`;
-
-// axios.get(apiUrl)
-//   .then(response => {
-//     console.log(response.data);
-//   })
-//   .catch(error => {
-//     console.error(error);
-//   });
 
 function fetchJobs(country = "us", resultsPerPage = 5, page = 1) {
     const apiUrl = `https://api.adzuna.com/v1/api/jobs/${country}/search/${page}?app_id=${apiID}&app_key=${apiKey}&results_per_page=${resultsPerPage}`;
@@ -28,4 +18,30 @@ function fetchJobs(country = "us", resultsPerPage = 5, page = 1) {
         });
 }
 
-fetchJobs();
+export function getJob(country = "us", resultsPerPage = 5, page = 1) {
+    const queryParams = new URLSearchParams({
+        country, 
+        resultsPerPage, 
+        page
+    }).toString();
+
+    return fetch(`http://localhost:${PORT}/getJobs?${queryParams}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data) {            
+                console.log(data);
+                console.log("hello");
+                return data;
+            } else {
+                console.log('No data found');
+                return null;
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            return Promise.reject(error);
+        });
+}
+
+getJob();
+
