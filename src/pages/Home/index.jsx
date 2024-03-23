2b4b2af651e574948b4a6bad0fc18b8c580ca727
TheV123 <vishkamalk@gmail.com>
2024-03-23 18:17:50 -0400

    home style 1

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,7 +2,8 @@ import { signOut } from "firebase/auth";
 import { auth } from "../../Firebase";
 import { Link } from "react-router-dom";
 import InterviewModal from "../InterviewModal";
-import { Button } from "@mui/material";
+import VideoChatIcon from "@mui/icons-material/VideoChat";
+import { Button, Grid, Icon, Typography } from "@mui/material";
 import { useState } from "react";
 import Navbar from "../../components/Navbar";
 
@@ -24,20 +25,84 @@ const Home = () => {
     }
   };
   return (
-    <div>
+    <div className="container mx-auto">
       <Navbar signOut={handleLogout} auth={auth} />
+      <div className="container mx-auto rounded-lg overflow-hidden shadow-sm ">
+        <div className="py-8 px-4">
+          <Typography variant="h2" align="center" className="mb-4">
+            AI Interview Assistant!
+          </Typography>
+          <Typography variant="subtitle1" align="center" className="mb-8">
+            AI Interview Assistant helps you prepare for your interviews by
+            providing insights and feedback.
+          </Typography>
+          <div className="grid grid-cols-3 gap-4 mb-8">
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+              className="p-6 bg-gradient-to-br from-[#C5D5EA] to-[#7392B7] text-white"
+            >
+              <div>
+                <VideoChatIcon size={48} className="mb-4" />
+                <Typography variant="h5" className="mb-2 font-bold">
+                  Interview With AI!
+                </Typography>
+              </div>
+              <Typography variant="body1">
+                Train your interview skills using AI! Get live feedback on your question 
+              </Typography>
+            </Grid>
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+              className="p-6 bg-gradient-to-br from-green-400 to-blue-600 text-white"
+            >
+              <Icon size={48} className="mb-4" />
+              <Typography variant="h5" className="mb-2 font-bold">
+                Grid 2
+              </Typography>
+              <Typography variant="body1">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </Typography>
+            </Grid>
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+              className="p-6 bg-gradient-to-br from-yellow-400 to-red-600 text-white"
+            >
+              <Icon size={48} className="mb-4" />
+              <Typography variant="h5" className="mb-2 font-bold">
+                Grid 3
+              </Typography>
+              <Typography variant="body1">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </Typography>
+            </Grid>
+          </div>
+          <div className="flex justify-center">
+            <Button variant="contained" onClick={handleOpen}>
+              New Interview
+            </Button>
+            <InterviewModal open={open} onClose={handleClose}>
+              <h2 id="modal-modal-title">Centered Modal</h2>
+              <p id="modal-modal-description">
+                This modal is centered in the middle of the screen.
+              </p>
+            </InterviewModal>
+          </div>
+        </div>
+      </div>
       <Link to="/login" onClick={handleLogout}>
         Logout
       </Link>
-      <Button variant="contained" onClick={handleOpen}>
-        New Interview
-      </Button>
-      <InterviewModal open={open} onClose={handleClose}>
-        <h2 id="modal-modal-title">Centered Modal</h2>
-        <p id="modal-modal-description">
-          This modal is centered in the middle of the screen.
-        </p>
-      </InterviewModal>
     </div>
   );
 };
