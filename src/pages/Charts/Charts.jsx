d24e0790484f02eb2f03117c74862b8d587032b8
ronishdua <ronishdua@gmail.com>
2024-03-23 23:28:03 -0400

    added charts

---
+++ src/pages/Charts/Charts.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import CanvasJSReact from '@canvasjs/react-charts';
+
+const CanvasJS = CanvasJSReact.CanvasJS;
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+const Charts = () => {
+    const options = {
+        animationEnabled: true,
+        title: {
+            text: "Monthly Sales - 2017"
+        },
+        axisX: {
+            valueFormatString: "MMM"
+        },
+        axisY: {
+            title: "Sales (in USD)",
+            prefix: "$"
+        },
+        data: [{
+            yValueFormatString: "$#,###",
+            xValueFormatString: "MMMM",
+            type: "spline",
+            dataPoints: [
+                { x: new Date(2017, 0), y: 25060 },
+                { x: new Date(2017, 1), y: 27980 },
+                { x: new Date(2017, 2), y: 42800 },
+                { x: new Date(2017, 3), y: 32400 },
+                { x: new Date(2017, 4), y: 35260 },
+                { x: new Date(2017, 5), y: 33900 },
+                { x: new Date(2017, 6), y: 40000 },
+                { x: new Date(2017, 7), y: 52500 },
+                { x: new Date(2017, 8), y: 32300 },
+                { x: new Date(2017, 9), y: 42000 },
+                { x: new Date(2017, 10), y: 37160 },
+                { x: new Date(2017, 11), y: 38400 }
+            ]
+        }]
+    };
+
+    return (
+        <div>
+            <CanvasJSChart options={options} />
+            {/* You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods */}
+        </div>
+    );
+};
+
+export default Charts;
