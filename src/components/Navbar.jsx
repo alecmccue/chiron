8e1271e02bc8cba6cbf8cb8a26df61c5d794445e
TheV123 <vishkamalk@gmail.com>
2024-03-23 20:00:29 -0400

    navbar and home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,19 +19,19 @@ function Navbar({handleLogout}) {
   };
 
   return (
-    <nav className="bg-white border-gray-200">
+    <nav className="border-isabelline">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <span className="self-center text-2xl font-semibold whitespace-nowra">
+          <span className="self-center text-2xl font-semibold whitespace-nowra text-onyx">
             Chiron
           </span>
         </Link>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white ">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-onyx rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white ">
             <li>
               <Link
                 to="/"
-                className="block py-2 px-3 text-xl text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                className="block py-2 px-3 text-xl text-white bg-current rounded md:bg-transparent md:text-current md:p-0 md:dark:text-current"
                 aria-current="page"
               >
                 Home
@@ -40,7 +40,7 @@ function Navbar({handleLogout}) {
             <li>
               <Link
                 to="/"
-                className="block py-2 px-3 text-xl text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                className="block py-2 px-3 text-xl text-white bg-current rounded md:bg-transparent md:current-700 md:p-0 md:dark:text-current"
                 aria-current="page"
               >
                 Charts
